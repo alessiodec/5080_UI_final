6518fe00a46ec41d4ac30eab58a02fef0c6587f5
alessiodec <alessiodec03@gmail.com>
2025-02-26 16:43:27 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,15 @@ st.sidebar.title("Navigation")
 page = st.sidebar.radio("Go to", ["Home", "Data Analysis", "Optimisation", "Physical Relationship Analysis"])
 
 if page == "Data Analysis":
+    st.session_state["data_analysis_page"] = "main"  # Reset Data Analysis state
     import data_analysis
     data_analysis.data_analysis()
 elif page == "Optimisation":
+    st.session_state["optimisation_page"] = "optimisation"  # Reset Optimisation state
     import optimisation
     optimisation.run()
 elif page == "Physical Relationship Analysis":
+    st.session_state["physical_relationship_page"] = "Main Menu"  # Reset Physical Relationship Analysis state
     import physical_relationship_analysis
     physical_relationship_analysis.run()
 else:
