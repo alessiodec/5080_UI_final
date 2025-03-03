1a33f7289215c9165c44b90df9dbab73920a5f1e
alessiodec <alessiodec03@gmail.com>
2025-03-03 11:29:48 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 
-# Sidebar Navigation
+# SIDEBAR NAVIGATION
 st.sidebar.title("Navigation")
 page = st.sidebar.radio("Go to", ["Home", "Data Analysis", "Optimisation", "Physical Relationship Analysis"])
 
+# ROUTE TO PAGE
 if page == "Data Analysis":
     import data_analysis
     data_analysis.data_analysis()
@@ -14,6 +15,7 @@ elif page == "Physical Relationship Analysis":
     import physical_relationship_analysis
     physical_relationship_analysis.run()
 else:
+    # HOME PAGE TEXT
     st.title("Welcome to the Main Page")
     st.write("This UI incorporates the customizable areas of this project for a user to design and solve their own problems.")
     st.write("Please select a page from the sidebar.")
