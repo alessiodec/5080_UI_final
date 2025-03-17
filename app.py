2dabeb03cd3ec3b45a480b8b1c5e66bc4281098e
alessiodec <alessiodec03@gmail.com>
2025-03-17 10:53:37 +0000

    Update app.py

--- app.py
+++ app.py
@@ -22,10 +22,10 @@ else:
     st.title("Machine Learning-Enabled Optimisation of Industrial Flow Systems - UI Tool")
     st.write("""
 This tool will guide you through the ML framework developed to analyse two industrial energy flow systems.\n
-    Surrogate Modelling: \n
-    Multi-Objective Optimisation: \n
-    Symbollic Regression: \n
 The framework is composed of three sections: surrogate modelling, multi-objective optimisation, and symbolic regression.\n
+    **Surrogate Modelling**: \n
+    **Multi-Objective Optimisation**: \n
+    **Symbollic Regression**: \n
 The first flow system is for a heatsink, where the available dataset consists of two geometric input parameters that yield outputs for pressure drop and thermal resistance.\n
 The second flow system involves the multi-physics software Leeds COMSOL, used to predict corrosion rate and saturation ratio in geothermal steel pipes for five inputs.\n
 Please select a page from the sidebar.
