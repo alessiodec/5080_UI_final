2a2f16ed6e55cd0cc937f7f99562640940001cba
alessiodec <alessiodec03@gmail.com>
2025-03-17 10:47:27 +0000

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,6 @@ else:
 This tool will guide you through the ML framework developed to analyse two industrial energy flow systems.\n
 The framework is composed of three sections: surrogate modelling, multi-objective optimisation, and symbolic regression.\n
 The first flow system is for a heatsink, where the available dataset consists of two geometric input parameters that yield outputs for pressure drop and thermal resistance.\n
-The second flow system involves the multi-physics software Leeds COMSOL, used to predict corrosion rate and saturation ratio in geothermal steel pipes.\n
+The second flow system involves the multi-physics software Leeds COMSOL, used to predict corrosion rate and saturation ratio in geothermal steel pipes for five inputs.\n
 Please select a page from the sidebar.
     """)
