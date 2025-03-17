99f8472f94f220a6b306dff643a70418456dd008
alessiodec <alessiodec03@gmail.com>
2025-03-17 10:42:49 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,29 @@
 import streamlit as st
- 
- # Set page configuration first
- st.set_page_config(layout="wide")
- 
- # SIDEBAR NAVIGATION
- st.sidebar.title("Navigation")
- page = st.sidebar.radio("Go to", ["Home", "Data Analysis", "Optimisation", "Physical Relationship Analysis"])
- 
- # ROUTE TO PAGE
- if page == "Data Analysis":
-     import data_analysis
-     data_analysis.data_analysis()
- elif page == "Optimisation":
-     import optimisation
-     optimisation.run()
- elif page == "Physical Relationship Analysis":
-     import physical_relationship_analysis
-     physical_relationship_analysis.run()
- else:
-     # HOME PAGE TEXT
-     st.title("Machine Learning-Enabled Optimisation of Industrial Flow Systems - UI Tool")
-     st.write("This tool will guide you through the ML framework developed to analyse two industrial energy flow systems.")
-     st.write("The framework is composed of three sections: surrogate modelling, multi-objective optimisation, and symbollic regression.")
-     st.write("The first flow system is a for a heatsink, where the avaliable dataset consists of two geoemetric input parameters, and outputting pressure drop and thermal resistance.")
-     st.write("The second flow system is that of a multi-physics software, Leeds COMSOL, used to predict corrosion rate and saturation ratio in gheothermal steel pipes. ")
-     st.write("This tool guides you through the ML framework developed to analyse two industrial energy flow systems.")
-     st.write("The framework comprises three key sections: surrogate modelling, multi-objective optimisation, and symbolic regression.")
-     st.write("The first flow system pertains to a heatsink, with a dataset containing two geometric input parameters that yield outputs for pressure drop and thermal resistance.")
-     st.write("The second flow system utilises the multi-physics software Leeds COMSOL to predict corrosion rate and saturation ratio in geothermal steel pipes.")
-     st.write("Please select a page from the sidebar.")
+
+# Set page configuration first
+st.set_page_config(layout="wide")
+
+# SIDEBAR NAVIGATION
+st.sidebar.title("Navigation")
+page = st.sidebar.radio("Go to", ["Home", "Data Analysis", "Optimisation", "Physical Relationship Analysis"])
+
+# ROUTE TO PAGE
+if page == "Data Analysis":
+    import data_analysis
+    data_analysis.data_analysis()
+elif page == "Optimisation":
+    import optimisation
+    optimisation.run()
+elif page == "Physical Relationship Analysis":
+    import physical_relationship_analysis
+    physical_relationship_analysis.run()
+else:
+    # HOME PAGE TEXT
+    st.title("Machine Learning-Enabled Optimisation of Industrial Flow Systems - UI Tool")
+    st.write("""
+This tool will guide you through the ML framework developed to analyse two industrial energy flow systems.
+The framework is composed of three sections: surrogate modelling, multi-objective optimisation, and symbolic regression.
+The first flow system is for a heatsink, where the available dataset consists of two geometric input parameters that yield outputs for pressure drop and thermal resistance.
+The second flow system involves the multi-physics software Leeds COMSOL, used to predict corrosion rate and saturation ratio in geothermal steel pipes.
+Please select a page from the sidebar.
+    """)
