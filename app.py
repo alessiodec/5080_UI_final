9824fa715efc4ca5ddc37d04ceaffbc2e4c83db6
alessiodec <alessiodec03@gmail.com>
2025-03-17 11:00:10 +0000

    Update app.py

--- app.py
+++ app.py
@@ -23,9 +23,9 @@ else:
     st.write("""
 This tool will guide you through the ML framework developed to analyse two industrial energy flow systems.\n
 The framework is composed of three sections: surrogate modelling, multi-objective optimisation, and symbolic regression.\n
-**Surrogate Modelling**: Constructing a computationally efficient metamodel using neural networks to approximate complex system behaviour.\n
-**Multi-Objective Optimisation**: Balancing conflicting objectives to achieve a trade-off solution.\n
-**Symbollic Regression**: Utilises methods like genetic programming to automatically generate interpretable mathematical expressions that accurately capture the underlying data relationships. \n
+ - **Surrogate Modelling**: Constructing a computationally efficient metamodel using neural networks to approximate complex system behaviour.\n
+ - **Multi-Objective Optimisation**: Balancing conflicting objectives to achieve a trade-off solution.\n
+ - **Symbollic Regression**: Utilises methods like genetic programming to automatically generate interpretable mathematical expressions that accurately capture the underlying data relationships. \n
 The first flow system is for a heatsink, where the available dataset consists of two geometric input parameters that yield outputs for pressure drop and thermal resistance.\n
 The second flow system involves the multi-physics software Leeds COMSOL, used to predict corrosion rate and saturation ratio in geothermal steel pipes for five inputs.\n
 Please select a page from the sidebar.
