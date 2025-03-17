437c413bcfa2cbd06dc2bb8f2cc16dd19722c3ef
alessiodec <alessiodec03@gmail.com>
2025-03-17 11:10:58 +0000

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ page = st.sidebar.radio("Go to", ["Home", "Data Analysis // Surrogate Modelling"
 
 # ROUTE TO PAGE
 if page == "Data Analysis // Surrogate Modelling":
+    st.session_state["data_analysis_page"] = "main"  # Reset to main menu
     import data_analysis
     data_analysis.data_analysis()
 elif page == "Optimisation":
