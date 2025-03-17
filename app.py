0a6135b4165144ddbda0dbae84ec8dfdca27a4b7
alessiodec <alessiodec03@gmail.com>
2025-03-17 10:40:01 +0000

    Update app.py

--- app.py
+++ app.py
@@ -18,10 +18,10 @@ elif page == "Physical Relationship Analysis":
     import physical_relationship_analysis
     physical_relationship_analysis.run()
 else:
-    # HOME PAGE TEXT
-    st.title("Machine Learning-Enabled Optimisation of Industrial Flow Systems - UI Tool")
-    st.write("This tool guides you through the ML framework developed to analyse two industrial energy flow systems.")
-    st.write("The framework comprises three key sections: surrogate modelling, multi-objective optimisation, and symbolic regression.")
-    st.write("The first flow system pertains to a heatsink, with a dataset containing two geometric input parameters that yield outputs for pressure drop and thermal resistance.")
-    st.write("The second flow system utilises the multi-physics software Leeds COMSOL to predict corrosion rate and saturation ratio in geothermal steel pipes.")
-    st.write("Please select a page from the sidebar.")
+    st.write("""
+    Welcome! This tool guides you through the ML framework developed to analyse two industrial energy flow systems.
+    The framework comprises three key sections: surrogate modelling, multi-objective optimisation, and symbolic regression.
+    The first flow system pertains to a heatsink, with a dataset containing two geometric input parameters that yield outputs for pressure drop and thermal resistance.
+    The second flow system utilises the multi-physics software Leeds COMSOL to predict corrosion rate and saturation ratio in geothermal steel pipes.
+    Please select a page from the sidebar.
+    """)
