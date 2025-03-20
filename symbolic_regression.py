e1ff28d55e8e0e3d9305e7a3e5104c68f5c24aae
alessiodec <alessiodec03@gmail.com>
2025-03-20 12:20:50 +0000

    Update symbolic_regression.py

--- symbolic_regression.py
+++ symbolic_regression.py
@@ -5,7 +5,14 @@ def run():
     st.title("Symbolic Regression")
     st.write("This page allows you to configure and run a symbolic regression algorithm. [Explanation placeholder for the page]")
 
-    # If no dataset has been selected, show the dataset selection buttons.
+    # If a dataset is selected, always show the "Return to Dataset Selection" button at the top.
+    if 'dataset' in st.session_state:
+        if st.button("Return to Dataset Selection"):
+            st.session_state.pop('dataset', None)
+            st.session_state.pop('output', None)
+            st.experimental_rerun()
+
+    # Dataset selection: if no dataset is selected, show dataset buttons.
     if 'dataset' not in st.session_state:
         st.write("Please Select a Dataset:")
         col1, col2 = st.columns(2)
@@ -13,8 +20,7 @@ def run():
             st.session_state['dataset'] = 'corrosion'
         if col2.button("Heatsink"):
             st.session_state['dataset'] = 'heatsink'
-
-    # If a dataset has been selected, show the output variable selection.
+    # If a dataset has been selected, show output variable selection.
     else:
         st.write(f"Dataset selected: {st.session_state['dataset'].capitalize()}")
         st.write("Please Select an Output Variable of Choice:")
@@ -31,28 +37,17 @@ def run():
             if col2_out.button("Thermal Resistance"):
                 st.session_state['output'] = 'thermal_resistance'
 
-    # Once both the dataset and output variable are selected, show the regression input fields.
+    # Once both dataset and output variable are selected, show regression input fields.
     if 'dataset' in st.session_state and 'output' in st.session_state:
         st.write("Please Select the Regression Inputs:")
         population_size = st.number_input("Population Size", value=100, step=1)
         population_retention_size = st.number_input("Population Retention Size", value=50, step=1)
         number_of_iterations = st.number_input("Number of Iterations", value=10, step=1)
 
-    # Navigation buttons
-    # If a dataset is selected, show both "Return to Dataset Selection" and "Go To Home" side by side.
-    if 'dataset' in st.session_state:
-        col_return, col_home = st.columns(2)
-        if col_return.button("Return to Dataset Selection"):
-            st.session_state.pop('dataset', None)
-            st.session_state.pop('output', None)
-            st.experimental_rerun()
-        if col_home.button("Go To Home"):
-            st.session_state.pop('dataset', None)
-            st.session_state.pop('output', None)
-            st.experimental_set_query_params(page="Home")
-            st.experimental_rerun()
-    # If no dataset is selected, only show the "Go To Home" button.
-    else:
-        if st.button("Go To Home"):
-            st.experimental_set_query_params(page="Home")
-            st.experimental_rerun()
+    # Always show the "Go To Home" button at the bottom.
+    if st.button("Go To Home"):
+        st.session_state.pop('dataset', None)
+        st.session_state.pop('output', None)
+        st.experimental_set_query_params(page="Home")
+        st.experimental_rerun()
+
