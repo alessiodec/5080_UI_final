c7314507f686d47cb224ce0223f15d2c4245bca4
alessiodec <alessiodec03@gmail.com>
2025-03-21 13:12:19 +0000

    Update config.py

--- functions/symbolic_regression_files/config.py
+++ functions/symbolic_regression_files/config.py
@@ -1,5 +1,7 @@
 # functions/symbolic_regression_files/config.py
 
+import streamlit as st
+
 # --- THRESHOLDS ----
 FIT_THRESHOLD = 100
 COMPLEXITY_MAX_THRESHOLD = 400
@@ -11,32 +13,35 @@ POPULATION_RETENTION_SIZE = 300
 TORNEMENT_SIZE = 3
 
 # --- SETTINGS --- 
-DATASET = 'BENCHMARK'  # Options: 'CORROSION', 'HEATSINK', or 'BENCHMARK'
-USE_RMSE = False       # If False, use 1-R^2 as fitness metric
-USE_SIMPLIFICATION = True
+DATASET = 'BENCHMARK'  # 'CORROSION', 'HEATSINK', or 'BENCHMARK' – used for having the same code...
+USE_RMSE = False       # if False then we use 1-R^2 
+USE_SIMPLIFICATION = True  # if True then we use the simplification code
 
-DISPLAY_ERROR_MESSAGES = False
-DISPLAY_SIMPLIFY_ERROR_MESSAGES = False
+DISPLAY_ERROR_MESSAGES = False         # Set to True for debugging
+DISPLAY_SIMPLIFY_ERROR_MESSAGES = False  # For simplification debugging
 
-TORN_SELECTION_METHOD = 'pareto'
-MATE_MUTATE_SELECTION_METHOD = 'all'
+TORN_SELECTION_METHOD = 'pareto'    # 'pareto' for Pareto-optimal or 'fitness' for fittest
+MATE_MUTATE_SELECTION_METHOD = 'all'  # 'pareto', 'fitness', or 'all' – all will be carried forward
 
-VERBOSE = 1
-DISPLAY_INTERVAL = 200
+VERBOSE = 1           # Whether to display progress messages
+DISPLAY_INTERVAL = 200  # How often to display progress
 
-# --- Reading intervals and thresholds ---
+# --- Reading Intervals ---
 PARETO_INDEX_INTERVAL = 5
 SIMPLIFICATION_INDEX_INTERVAL = 10
 EARLY_STOPPING_THRESHOLD = 10
 FITNESS_REDUCTION_THRESHOLD = 4
 FITNESS_REDUCTION_FACTOR = 0.9
 
-# Global toolbox and primitive set (to be set by EngineDict)
+# Variables for function generation in other modules (e.g., EngineDict)
 TOOLBOX = None
 PSET = None
 
-# Data variables (to be loaded/set later)
+# Data placeholders (set later in the notebook or application)
 X = None
 y = None
 std_y = None
 mean_y = None
+
+if __name__ == "__main__":
+    st.write("This is the config file for the symbolic regression system. It holds all global parameters and thresholds.")
