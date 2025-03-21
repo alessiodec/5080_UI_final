351cae774801a5a4e6d194c1e93a8df7a5f07b10
alessiodec <alessiodec03@gmail.com>
2025-03-21 13:09:55 +0000

    Update Plotting.py

--- functions/symbolic_regression_files/Plotting.py
+++ functions/symbolic_regression_files/Plotting.py
@@ -2,10 +2,17 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from . import config
-from . import EngineDict as Engine
+import streamlit as st
+import config
+import EngineDict as Engine
 
 def plot_pareto(population, yscale='log', ymax=None, ymin=None):
+    """
+    Plots the Pareto front and entire population.
+    
+    Returns:
+        A matplotlib figure object that can be rendered via st.pyplot().
+    """
     pareto_front = Engine.return_pareto_front(population)
     pareto_plot_data = []
     for individual in pareto_front:
@@ -19,27 +26,39 @@ def plot_pareto(population, yscale='log', ymax=None, ymin=None):
     
     utopia_point = [min(population_plot_data[:, 1]), min(population_plot_data[:, 0])]
     if config.VERBOSE:
-        print(utopia_point)
+        st.write("Utopia point:", utopia_point)
     
-    plt.figure()
-    plt.scatter(population_plot_data[:, 1], population_plot_data[:, 0], s=15, label="Population")
-    plt.scatter(pareto_plot_data[:, 1], pareto_plot_data[:, 0], s=15, label="Pareto Front")
-    plt.scatter(utopia_point[0], utopia_point[1], label='Utopia')
-    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-    plt.ylabel("Fitness $1-R^2$")
-    plt.xlabel("Complexity")
+    fig, ax = plt.subplots()
+    ax.scatter(population_plot_data[:, 1], population_plot_data[:, 0], s=15, label="Population")
+    ax.scatter(pareto_plot_data[:, 1], pareto_plot_data[:, 0], s=15, label="Pareto Front")
+    ax.scatter(utopia_point[0], utopia_point[1], label='Utopia')
+    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    ax.set_ylabel("Fitness $1-R^2$")
+    ax.set_xlabel("Complexity")
+    
     if yscale == 'log':
-        plt.yscale("log")
+        ax.set_yscale("log")
+    
     if (ymax is not None and ymin is not None):
-        plt.ylim(ymin, ymax)
+        ax.set_ylim(ymin, ymax)
+    
+    return fig
 
 def plot_next_gen_parents(population, yscale='log', ymax=None, ymin=None):
+    """
+    Plots the population, parent candidate points, and Pareto front for the next generation.
+    
+    Returns:
+        A matplotlib figure object that can be rendered via st.pyplot().
+    """
     pareto_front = Engine.return_pareto_front(population)
     top_n = Engine.generate_new_generation_NSGA_2(config.POPULATION_RETENTION_SIZE, population)
+    
     top_n_data = []
-    for i, individual in enumerate(top_n):
+    for individual in top_n:
         top_n_data.append((individual['fitness'], individual['complexity']))
     top_n_data = np.array(top_n_data)
+    
     pareto_plot_data = []
     for individual in pareto_front:
         pareto_plot_data.append((individual['fitness'], individual['complexity']))
@@ -52,17 +71,21 @@ def plot_next_gen_parents(population, yscale='log', ymax=None, ymin=None):
     
     utopia_point = [min(population_plot_data[:, 1]), min(population_plot_data[:, 0])]
     if config.VERBOSE:
-        print(utopia_point)
+        st.write("Utopia point:", utopia_point)
     
-    plt.figure()
-    plt.scatter(population_plot_data[:, 1], population_plot_data[:, 0], s=15, label="Population")
-    plt.scatter(top_n_data[:, 1], top_n_data[:, 0], s=15, label="Parent Candidates")
-    plt.scatter(pareto_plot_data[:, 1], pareto_plot_data[:, 0], s=15, label="Pareto Front")
-    plt.scatter(utopia_point[0], utopia_point[1], label='Utopia')
-    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-    plt.ylabel("Fitness $1-R^2$")
-    plt.xlabel("Complexity")
+    fig, ax = plt.subplots()
+    ax.scatter(population_plot_data[:, 1], population_plot_data[:, 0], s=15, label="Population")
+    ax.scatter(top_n_data[:, 1], top_n_data[:, 0], s=15, label="Parent Candidates")
+    ax.scatter(pareto_plot_data[:, 1], pareto_plot_data[:, 0], s=15, label="Pareto Front")
+    ax.scatter(utopia_point[0], utopia_point[1], label='Utopia')
+    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    ax.set_ylabel("Fitness $1-R^2$")
+    ax.set_xlabel("Complexity")
+    
     if yscale == 'log':
-        plt.yscale("log")
+        ax.set_yscale("log")
+    
     if (ymax is not None and ymin is not None):
-        plt.ylim(ymin, ymax)
+        ax.set_ylim(ymin, ymax)
+    
+    return fig
