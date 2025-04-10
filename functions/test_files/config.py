548e688a86118a515ce76ba9026551a4ccbb5701
alessiodec <alessiodec03@gmail.com>
2025-04-10 12:57:22 +0100

    Update config.py

--- functions/test_files/config.py
+++ functions/test_files/config.py
@@ -1,5 +1,7 @@
-# --- THRESHOLDS ----
-FIT_THRESHOLD = 100
+# functions/test_files/config.py
+
+# --- THRESHOLDS ---
+FIT_THRESHOLD = 10             # Lowered to speed up initial population acceptance
 COMPLEXITY_MAX_THRESHOLD = 400
 COMPLEXITY_MIN_THRESHOLD = 1
 
@@ -9,27 +11,30 @@ POPULATION_RETENTION_SIZE = 300
 TORNEMENT_SIZE = 3
 
 # --- SETTINGS ---
-DATASET = 'BENCHMARK'  # 'CORROSION', 'HEATSINK', or 'BENCHMARK'
-USE_RMSE = False      # if False, we use 1-R^2
+# DATASET will be set dynamically (CORROSION or HEATSINK)
+DATASET = 'BENCHMARK'
+USE_RMSE = False
 USE_SIMPLIFICATION = True
 
 DISPLAY_ERROR_MESSAGES = False
 DISPLAY_SIMPLIFY_ERROR_MESSAGES = False
 
-TORN_SELECTION_METHOD = 'pareto'         # 'pareto' or 'fitness'
-MATE_MUTATE_SELECTION_METHOD = 'all'      # 'pareto', 'fitness', or 'all'
+# For selection and mating:
+SELECTION_METHOD = 'pareto'     # Options: 'pareto' or 'fitness'
+# (In some of your older code, this was referenced as TORN_SELECTION_METHOD; adjust accordingly.)
+MATE_MUTATE_SELECTION_METHOD = 'all'
 
 VERBOSE = 1
 DISPLAY_INTERVAL = 200
 
-# --- Reading Intervals ---
+# --- Iteration Parameters ---
 PARETO_INDEX_INTERVAL = 5
-SIMPLIFICATION_INDEX_INTERVAL = 10
-EARLY_STOPPING_THRESHOLD = 10
-FITNESS_REDUCTION_THRESHOLD = 4
-FITNESS_REDUCTION_FACTOR = 0.9
+SIMPLIFICATION_INDEX_INTERVAL = 20
+EARLY_STOPPING_THRESHOLD = 20
+FITNESS_REDUCTION_THRESHOLD = 5
+FITNESS_REDUCTION_FACTOR = 0.8
 
-# Global variables for data
+# Global variables for data; these will be set at runtime.
 TOOLBOX = None
 PSET = None
 X = None
